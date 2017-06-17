eb8d7c5fe853f072190b43b09afa5568bc0f6c7f
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-16 19:32:02 -0600

    Added ability to select a task in the list and its details be shown in the details section. The details section still needs to be fleshed out.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var item = Vue.component( 'task', {
     template: `
-                <li class="task-in-list">
+                <li class="task-in-list" v-on:click="selectTask(task)">
                     <h2>{{task.title}}</h2>
                     <h3>{{task.time}}</h3>
                     <h3>{{task.locationName}}</h3>
@@ -13,20 +13,16 @@ var item = Vue.component( 'task', {
     },
     props: ['task'],
     computed: {
-
+        
     },
     methods: {
         removeTask: function ( task ) {
-            console.log( "Removing from component", task );
             this.$emit( 'remove-task', task );
+        },
+        selectTask: function ( task ) {
+            console.log("Select Task from component")
+            this.$emit( 'select-task', task );
         }
-        //check to see if not enough money
-        //buyItem: function ( item ) {
-        //    this.$emit( 'buy-item', item )
-        //},
-        //sellItem: function ( item ) {
-        //    this.$emit( 'sell-item', item )
-        //}
     }
 });
 
@@ -34,12 +30,14 @@ var item = Vue.component( 'task', {
 var mainAppVm = new Vue( {
     el: '#app',
     data: {
+
+        selectedTask: {},
         taskList: [
             //Create Task
             {
                 id:1,
                 title: "Pick Up Package",
-                descrition: "Pick up package from post office",
+                description: "Pick up package from post office",
                 date: "6/18/2017",
                 time: "1:30 pm",
                 duration: 0.25,
@@ -49,7 +47,7 @@ var mainAppVm = new Vue( {
             {
                 id: 2,
                 title: "Get Beer",
-                descrition: "Get beer for Micahl's party",
+                description: "Get beer for Micahl's party",
                 date: "6/18/2017",
                 time: "4:30 pm",
                 duration: 0.25,
@@ -59,7 +57,7 @@ var mainAppVm = new Vue( {
             {
                 id: 3,
                 title: "Micahl's Party",
-                descrition: "Micahl's birthday party",
+                description: "Micahl's birthday party",
                 date: "6/18/2017",
                 time: "7:00 pm",
                 duration: 0.25,
@@ -69,7 +67,7 @@ var mainAppVm = new Vue( {
             {
                 id: 4,
                 title: "Micahl's Party",
-                descrition: "Micahl's birthday party",
+                description: "Micahl's birthday party",
                 date: "6/18/2017",
                 time: "7:00 pm",
                 duration: 0.25,
@@ -77,32 +75,42 @@ var mainAppVm = new Vue( {
                 address: "1117 Pearl St, Boulder, CO 80302"
             }
 
-        ]
+        ],
     },
 
     computed: {
+        //selectedTask: function () {
+        //    return this.taskList[0];
+        //}
     },
     methods: {
         newTask: function () {
-            this.taskList.push( {
+
+            var newTask = {
                 id: this.taskList.length,
                 title: "Untitled Task",
-                descrition: "",
+                description: "",
                 date: "",
                 time: "",
                 duration: 0,
                 locationName: "",
                 address: ""
-            })
+            };
+            this.taskList.push( newTask );
+            this.selectedTask = newTask;
+
             //create new task and push onto array
             //make it the selected task
         },
         removeTask: function ( task ) {
-            //find in list
-            //remove from it
-            console.log("Remove task", task );
             this.taskList.splice( $.inArray( task, this.taskList ), 1 );
-
+        },
+        selectTask: function ( task ) {
+            console.log( "Select class" );
+            this.selectedTask = task;
         }
+    },
+    created: function () {
+        this.selectedTask = this.taskList[0];
     }
 })
