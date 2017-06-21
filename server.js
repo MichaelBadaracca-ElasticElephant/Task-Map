6cd5cfdfc148ef635fb057ff3dfd4459fe544171
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-21 13:09:02 -0600

    Added middleware to serve homepage with a /. Added logging middleware.

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ var app = express();
 
 app.use( express.static( './' ) );
 
+app.use( function ( req, res, next ) {
+    console.log( req.url );
+    next();
+})
 
+
+app.get( "/", function ( req, res ) {
+    res.status( 200 );
+    res.sendFile( "./mapTask.html", { "root": __dirname });
+});
 
 //The Google Maps API key is retrieved from the server to keep it out of source control and less accessible on the client side
 app.get( "/googleMapsApiKey", function ( req, res ) {
@@ -17,4 +26,4 @@ app.get( "/googleMapsApiKey", function ( req, res ) {
 //TODO: Make a 404 route
 //TODO: Make a 500 error route
 
-app.listen( 80 );
+app.listen( 3000 );
