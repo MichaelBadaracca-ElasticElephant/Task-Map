fd733a17c72b852af36d5793a3cb7241856111cb
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-21 21:24:32 -0600

    Moved task component into its own js file

--- main.js
+++ main.js
@@ -1,40 +1,3 @@
-var item = Vue.component( 'task', {
-    template: `
-                <div class="task-in-list" v-on:click="selectTask(task)" v-bind:class='{selected:task.isSelected}'>          
-                    <h2>{{i + 1}}. {{task.title}}</h2>    
-                    <div class="task-info">
-                        <h4>{{time()}} on {{task.dateTime.toDateString()}}</h4>
-                        <h4>{{task.locationName}}</h4>
-                        <h4>{{task.address}}</h4>
-                    </div>
-                    <div class="task-controls">
-                        <button class="glyphicon glyphicon-trash btn btn-danger" v-on:click="removeTask(task)"></button>
-                        <button class="glyphicon glyphicon-pencil btn btn-info" v-on:click=""></button>
-                    </div>
-                    
-                </div>
-              `,
-    props: ['task','i'],
-    data: function () {
-        return {}
-    },
-    
-    computed: {
-
-    },
-    methods: {
-        removeTask: function ( task ) {
-            this.$emit( 'remove-task', task );
-        },
-        selectTask: function ( task ) {
-            this.$emit( 'select-task', task );
-        },
-        time: function () {
-            var time = formatAMPM( this.task.dateTime );
-            return time;
-        }
-    }
-});
 
 var mainAppVm = new Vue( {
     el: '#app',
@@ -170,18 +133,6 @@ var mainAppVm = new Vue( {
         }
     },
     methods: {
-        getMapScript: function () {
-            $.get( "/googleMapsApiKey", function ( googleMapsApiKey ) {
-                var googleMapsApiScript = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places&callback=mainAppVm.initMap`;
-                $.getScript( googleMapsApiScript, function () {
-                    console.log( "Google maps script loaded" );
-                    mainAppVm.initAutocomplete();
-                });
-            });
-        },
-        testFunction: function () {
-            console.log( "Test function" );
-        },
         //*** MAPS START ***
         initMap: function () {
 
@@ -313,6 +264,10 @@ var mainAppVm = new Vue( {
         getMapScript();
     }
 })
+
+
+//****************** END OF VUE ******************
+
 
 $( document ).ready( function () {
     updateLocalTimePicker( mainAppVm.selectedTask.dateTime );
---
+++ scripts/vueTaskComponent.js
@@ -0,0 +1,37 @@
+var item = Vue.component( 'task', {
+    template: `
+                <div class="task-in-list" v-on:click="selectTask(task)" v-bind:class='{selected:task.isSelected}'>          
+                    <h2>{{i + 1}}. {{task.title}}</h2>    
+                    <div class="task-info">
+                        <h4>{{time()}} on {{task.dateTime.toDateString()}}</h4>
+                        <h4>{{task.locationName}}</h4>
+                        <h4>{{task.address}}</h4>
+                    </div>
+                    <div class="task-controls">
+                        <button class="glyphicon glyphicon-trash btn btn-danger" v-on:click="removeTask(task)"></button>
+                        <button class="glyphicon glyphicon-pencil btn btn-info" v-on:click=""></button>
+                    </div>
+                    
+                </div>
+              `,
+    props: ['task', 'i'],
+    data: function () {
+        return {}
+    },
+
+    computed: {
+
+    },
+    methods: {
+        removeTask: function ( task ) {
+            this.$emit( 'remove-task', task );
+        },
+        selectTask: function ( task ) {
+            this.$emit( 'select-task', task );
+        },
+        time: function () {
+            var time = formatAMPM( this.task.dateTime );
+            return time;
+        }
+    }
+});
